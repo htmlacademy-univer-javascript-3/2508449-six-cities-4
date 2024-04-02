9e4f6474791d7e40aad9de3328d8f8537b67a7c1
SomeUnknown <73284147+SomeUnknown@users.noreply.github.com>
2024-04-02 23:55:32 +0500

    Done module2-task1

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { App } from './app';
+import { type Place } from './types';
+
+const cards: Place[] = [
+  {
+    name: 'Paris',
+    type: 'apartment',
+    preview: 'img/apartment-01.jpg',
+    rating: 4,
+    valuePerNight: 120,
+    isPremium: true,
+    isBookmarked: false,
+  },
+  {
+    name: 'Wood and stone place',
+    type: 'room',
+    preview: 'img/room.jpg',
+    rating: 4,
+    valuePerNight: 80,
+    isPremium: false,
+    isBookmarked: true,
+  },
+  {
+    name: 'Canal View Prinsengracht',
+    type: 'apartment',
+    preview: 'img/apartment-02.jpg',
+    rating: 4,
+    valuePerNight: 132,
+    isPremium: false,
+    isBookmarked: false,
+  },
+  {
+    name: 'Nice, cozy, warm big bed apartment',
+    type: 'apartment',
+    preview: 'img/apartment-03.jpg',
+    rating: 5,
+    valuePerNight: 180,
+    isPremium: true,
+    isBookmarked: false,
+  },
+  {
+    name: 'Wood and stone place',
+    type: 'room',
+    preview: 'img/room.jpg',
+    rating: 4,
+    valuePerNight: 80,
+    isPremium: false,
+    isBookmarked: true,
+  },
+];
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -7,6 +57,6 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <h1>Hello, World!</h1>
+    <App cards={cards} />
   </React.StrictMode>
 );
