5104476efd58f658564f99a6d2b94c1df811aa95
SomeUnknown <73284147+SomeUnknown@users.noreply.github.com>
2024-04-02 23:48:40 +0500

    Done

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,39 @@
-import { FC } from 'react';
 import { Main } from '../pages/Main';
-import type { Place } from '../types';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { cards } from '../mock/mock';
+import { Favorites } from '../pages/Favorites';
+import { Login } from '../pages/Login';
+import { Offer } from '../pages/Offer';
+import { Layout } from './Layout';
+import { ProtectedRoute } from '../components/ProtectedRoute';
 
-export const App: FC<{ cards?: Place[] }> = (props) => (
-  <Main cards={props.cards} />
+
+export const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Main cards={cards} />} />
+      </Route>
+      <Route
+        path="offer/:id"
+        element={<Offer />}
+      />
+      <Route
+        path="/favorites"
+        element={
+          <ProtectedRoute user=''>
+            <Favorites />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/login"
+        element={<Login />}
+      />
+      <Route path="*" element={
+        <><h1>Ошибка 404. Запрашиваемой страницы не существует.</h1><h2><a href="/">Вернуться на главную</a></h2></>
+      }
+      />
+    </Routes>
+  </BrowserRouter>
 );
