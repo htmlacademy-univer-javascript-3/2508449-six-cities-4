9a1ff426e70066c77400756772565c137b83fe69
SomeUnknown <73284147+SomeUnknown@users.noreply.github.com>
2024-04-22 14:46:21 +0500

    Done module5-task2

--- src/widgets/NearPlaces/NearPlaces.tsx
+++ src/widgets/NearPlaces/NearPlaces.tsx
@@ -1,28 +1,30 @@
 import { FC } from 'react';
 
-import { offers } from '../../shared/mocks';
-
 import { type Offer, OfferCard } from '../../entities';
 
 type NearPlacesProps = {
-  initialPlaceId: string;
+  nearPlaces: Offer[];
 };
 
-export const NearPlaces: FC<NearPlacesProps> = () => {
-  const data = offers.slice(1, 4) as Offer[];
-
+export const NearPlaces: FC<NearPlacesProps> = ({ nearPlaces }) => {
   return (
     <div className="container">
-      <section className="near-places places">
-        <h2 className="near-places__title">
-          Other places in the neighbourhood
-        </h2>
-        <div className="near-places__list places__list">
-          {data.map((offer) => (
-            <OfferCard className="near-places" offer={offer} key={offer.id} />
-          ))}
-        </div>
-      </section>
+      {nearPlaces.length > 0 && (
+        <section className="near-places places">
+          <h2 className="near-places__title">
+            Other places in the neighbourhood
+          </h2>
+          <div className="near-places__list places__list">
+            {nearPlaces.map((nearPlace) => (
+              <OfferCard
+                className="near-places"
+                offer={nearPlace}
+                key={nearPlace.id}
+              />
+            ))}
+          </div>
+        </section>
+      )}
     </div>
   );
 };
